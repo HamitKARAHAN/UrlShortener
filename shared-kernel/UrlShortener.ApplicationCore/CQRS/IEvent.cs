e7fed5e8576749e7794d43adbbc16debf2675c0e
HamitKARAHAN <hamitkarahan10@gmail.com>
2023-12-13 21:40:05 +0300

    Abstraction fix

--- shared-kernel/UrlShortener.ApplicationCore/CQRS/IEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿// <copyright file="IEvent.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
-namespace UrlShortener.ApplicationCore.CQRS;
-using MediatR;
-public interface IEvent : INotification
-{
-}
